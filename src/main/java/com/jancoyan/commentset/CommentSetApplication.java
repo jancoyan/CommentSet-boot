a9f42012f41adc07419618a9acf5b06e9b6ed23c
jancoyan <302848188@qq.com>
2021-08-21 19:45:08 +0800

    create

---
+++ src/main/java/com/jancoyan/commentset/CommentSetApplication.java
@@ -0,0 +1,13 @@
+package com.jancoyan.commentset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentSetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommentSetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jancoyan/commentset/CommentSetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jancoyan.commentset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentSetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
